debd4d3666265818521ec044ed3b11c4361d2f5d
OLDILJ <oldilj1@student.op.ac.nz>
2024-02-27 14:01:31 +1300

    Lab_1 Finished

---
+++ Programming1/Lab_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("* * * * * * * * * * * *");
+Console.WriteLine("* Logan J. Olding     *");
+Console.WriteLine("* 40 Beaconsfield RD  *");
+Console.WriteLine("* My favorite BIT     *");
+Console.WriteLine("* Subject is :idk yet *");
+Console.WriteLine("* * * * * * * * * * * *");
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.ReadLine();
