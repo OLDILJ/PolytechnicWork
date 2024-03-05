0e3486970569762c7c365b316ab3e718efaa70e2
OLDILJ <oldilj1@student.op.ac.nz>
2024-03-05 13:48:06 +1300

    Finished Lab 5

---
+++ Programming1/Lab_5/Program.cs
@@ -0,0 +1,39 @@
+﻿Random rand = new Random();
+string confirmation;
+int coinflip = rand.Next(1,3);
+Console.WriteLine("Heads or Tails?");
+Console.WriteLine("1. Head");
+Console.WriteLine("2. Tails");
+confirmation = Console.ReadLine();
+if (confirmation.Equals("1"))
+    {
+        Console.WriteLine("You Have Picked Heads");
+        if (coinflip == 1)
+            {
+            Console.WriteLine("You Win!");
+            //Console.WriteLine(coinflip); //check flip result
+            Console.ReadLine();
+            }   
+        else 
+            {
+            Console.WriteLine("You Lose");
+            //Console.WriteLine(coinflip); //check flip result
+            Console.ReadLine();
+            }   
+    }
+if (confirmation.Equals("2"))
+{
+    Console.WriteLine("You Have Picked Tails");
+    if (coinflip == 2)
+        {
+            Console.WriteLine("You Win!");
+            //Console.WriteLine(coinflip); //check flip result
+            Console.ReadLine();
+        }
+    else 
+        {
+            Console.WriteLine("You Lose");
+            //Console.WriteLine(coinflip); //check flip result
+            Console.ReadLine();
+        }   
+}
