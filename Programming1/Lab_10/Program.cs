538891eeedc4d2c7f449b631891734fc2ce0591f
OLDILJ <oldilj1@student.op.ac.nz>
2024-03-14 10:14:50 +1300

    lab_10 started

---
+++ Programming1/Lab_10/Program.cs
@@ -0,0 +1,21 @@
+﻿int i;
+Console.WriteLine("Counting up");
+for (i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Counting Down");
+for (i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.ReadLine();
+
+Console.WriteLine("for loop with characters");
+for (char j = 'a'; j<'g'l j++)
+{
+    Console.WriteLine(j);
+}
+
+Console.WriteLine("For loop with double");
