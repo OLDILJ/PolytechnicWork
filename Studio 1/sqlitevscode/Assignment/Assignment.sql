5dccb6a70294bfddbd93028a4b3e42b0a577dc7c
OLDILJ <oldilj1@student.op.ac.nz>
2024-03-28 16:28:31 +1300

    Started Fake Code Pass #1

--- Studio 1/sqlitevscode/Assignment/Assignment.sql
+++ Studio 1/sqlitevscode/Assignment/Assignment.sql
@@ -1,10 +1,54 @@
+-- database: :memory:
+
+///FAKE CODE VERSION BELOW
+
 create table Entity(
+Entity_ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+EquipmentSlot FOREIGN key not null,
+Name text,
+TeamID foreign key not null,
+Afflicted_Status_effects text,
+Equipment text
+);
+
+create table Team(
 Team Primary key not null,
-name text,
-turn_order foreign key not null,
-type text,
-engine_capacity integer,
-engine_power integer,
-dry_weight integer,
-price_2020 real
+Name text,
+Entity_ID foreign key not null,
+TeamID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+);
+
+create table BattleMaster(
+Battle_ID Primary key not null,
+Name text,
+Special_Event text,
+TeamID FOREIGN key not null,
+Afflicted_Status_effects text
+);
+
+create table Equipment(
+Equipment_ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+Name text,
+EquipmentSlot PRIMARY key not null,
+);
+
+create table StatModifier(
+Name text,
+StatName foreign key not null
+);
+
+create table Entity(
+Entity_ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+Name text,
+TeamID foreign key not null,
+Afflicted_Status_effects text,
+Equipment text
+);
+
+create table Entity(
+Entity_ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+Name text,
+TeamID foreign key not null,
+Afflicted_Status_effects text,
+Equipment text
 );
