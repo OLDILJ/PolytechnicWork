ec173f4e348a0bb63b770778a1130abc3a25b09c
OLDILJ <oldilj1@student.op.ac.nz>
2024-04-04 11:52:53 +1300

    begun lab13v2
    with minor adjustments to lab13v1

---
+++ Programming1/lab_13_v2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] loweralphabet = ("abcdefghijklmnopqrstuvwxyz");
+
+string[] upperalphabet = loweralphabet.ToUpper();
+
+int[] lowercount = new int[26]
+
+int[] uppercount = new int[26]
+
+/*
+Step 1 Create four strings, two for upper and lower case, two for position in respective alphabet 
+Step 2 Read a sentence given by the user
+Step 3 check how many times a part of either alphabet is contained in a sentence 
+Step 4 increment the respective count in the right array.
+Step 5 Print to screen
+
+*/
