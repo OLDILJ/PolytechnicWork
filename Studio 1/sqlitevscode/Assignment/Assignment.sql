620fa111b4e4aaf402a4b72fa1705f0890432e29
OLDILJ <oldilj1@student.op.ac.nz>
2024-04-09 16:30:24 +1200

    Added Alot of stuff
    BattleMaster, StatusStatModifier, Team, and some funky queries.

--- Studio 1/sqlitevscode/Assignment/Assignment.sql
+++ Studio 1/sqlitevscode/Assignment/Assignment.sql
@@ -7,6 +7,32 @@ PRAGMA writable_schema = 0;
 VACUUM;
 pragma integrity_check;
 
+-- test code environment
+
+
+-- big math time
+UPDATE Status
+SET Max_health = CAST(CAST(Max_health AS INTEGER) + (
+    SELECT SUM(CAST(StatModifier.Max_health_modifier AS INTEGER))
+    FROM StatusStatModifier
+    JOIN StatModifier ON StatusStatModifier.StatModValue = StatModifier.StatModValue
+    WHERE StatusStatModifier.Status_ID = Status.Status_ID
+) AS TEXT)
+WHERE Status_ID = (SELECT Status_ID FROM Entity WHERE Entity_ID = 1);
+
+SELECT Status.Max_health
+FROM Entity
+JOIN Status ON Entity.Status_ID = Status.Status_ID
+WHERE Entity.Entity_ID = 1;
+
+UPDATE Status
+SET Max_health = CAST(CAST(Max_health AS INTEGER) - (
+    SELECT SUM(CAST(StatModifier.Max_health_modifier AS INTEGER))
+    FROM StatusStatModifier
+    JOIN StatModifier ON StatusStatModifier.StatModValue = StatModifier.StatModValue
+    WHERE StatusStatModifier.Status_ID = Status.Status_ID
+) AS TEXT)
+WHERE Status_ID = (SELECT Status_ID FROM Entity WHERE Entity_ID = 1);
 
 -- random stuff
 
@@ -15,9 +41,61 @@ select * from Equipment;
 select * from Status;
 select * from StatModifier;
 select * from Entity;
+select * from Team;
+SELECT * FROM StatusStatModifier;
+SELECT * FROM BattleMaster;
+
+-- Check current/ max hp of 'player1' entity
+SELECT Status.Current_HP, Status.Max_health FROM Entity INNER JOIN Status ON Entity.Status_ID = Status.Status_ID WHERE Entity.Name = 'Player1';
+
+SELECT Current_HP FROM Entity INNER JOIN Status WHERE Entity.Name = 'Player1'; -- gives me all current hp without caring for STATUS_ID
+
+
+-- the GIGA STATEMENT
+
+UPDATE Status
+SET Max_health = CAST(CAST(Max_health AS INTEGER) + (
+    SELECT SUM(CAST(StatModifier.Max_health_modifier AS INTEGER))
+    FROM StatusStatModifier
+    JOIN StatModifier ON StatusStatModifier.StatModValue = StatModifier.StatModValue
+    WHERE StatusStatModifier.Status_ID = Status.Status_ID
+) AS TEXT)
+WHERE Status_ID = (SELECT Status_ID FROM Entity WHERE Entity_ID = 1);
+
+SELECT Status.Max_health
+FROM Entity
+JOIN Status ON Entity.Status_ID = Status.Status_ID
+WHERE Entity.Entity_ID = 1;
+
+UPDATE Status
+SET Max_health = CAST(CAST(Max_health AS INTEGER) - (
+    SELECT SUM(CAST(StatModifier.Max_health_modifier AS INTEGER))
+    FROM StatusStatModifier
+    JOIN StatModifier ON StatusStatModifier.StatModValue = StatModifier.StatModValue
+    WHERE StatusStatModifier.Status_ID = Status.Status_ID
+) AS TEXT)
+WHERE Status_ID = (SELECT Status_ID FROM Entity WHERE Entity_ID = 1);
+
+-- BattleMaster sample data query
+SELECT BattleMaster.Battle_ID, BattleMaster.Name AS BattleName, BattleMaster.Special_Event, Team.Name AS TeamName
+FROM BattleMaster
+JOIN Team ON BattleMaster.TeamID = Team.TeamID;
 
 -- Data Inserts
 
+-- Battle Master
+INSERT INTO BattleMaster (Battle_ID, Name, Special_Event, TeamID, Afflicted_Status_effects)
+VALUES
+(1, 'Battle1', 'Ambush', 1, 'Stunned'),
+(2, 'Battle2', 'Boss Fight', 2, 'Poisoned'),
+(3, 'Battle3', 'Siege', 1, 'Slowed'),
+(4, 'Battle4', 'Normal Battle', 1, null);
+
+
+-- Team
+INSERT INTO Team (Name, Entity_ID) VALUES 
+('Team1', 1), -- Player1
+('Team2', 2); -- Enemy1
 -- Equipment
 insert into Equipment (Name, EquipmentSlot) VALUES ('Sword', 'Weapon');
 insert into Equipment (Name, EquipmentSlot) VALUES ('Shield', 'Offhand');
@@ -46,6 +124,12 @@ VALUES
 (5, 'Luck Charm', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '+2', NULL, NULL, NULL, NULL),
 (6, 'Defense Potion', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '+10', NULL);
 
+-- Stat Mod Mod
+INSERT INTO StatusStatModifier (Status_ID, StatModValue)
+VALUES 
+(1, 1), -- Status 1, StatModifier 1
+(1, 2), -- Status 1, StatModifier 2
+(2, 2); -- Status 2, StatModifier 2
 
 -- Entity
 INSERT INTO Entity (Name, Equipment, Status_ID)
@@ -55,6 +139,7 @@ INSERT INTO Entity (Name, Equipment, Status_ID)
 VALUES ('Enemy1', 'None', 2);
 
 -- Main Code
+
 create table Equipment(
 Equipment_ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 Name text,
@@ -77,9 +162,7 @@ Accuracy text,
 Defense text,
 Evasion text
 );
-/*
-ADD A JOINING TABLE BETWEEN STATUS AND STAT MODIFIER
-*/
+
 CREATE TABLE StatModifier(
     StatModValue INTEGER PRIMARY KEY,
     Name TEXT,
@@ -95,7 +178,7 @@ CREATE TABLE StatModifier(
     Attack_modifier TEXT,
     Accuracy_modifier TEXT,
     Defense_modifier TEXT,
-    Evasion_modifier TEXT,
+    Evasion_modifier TEXT/*
     FOREIGN KEY (Max_health_modifier) REFERENCES Status(Max_health),
     FOREIGN KEY (Current_HP_modifier) REFERENCES Status(Current_HP),
     FOREIGN KEY (Max_MP_modifier) REFERENCES Status(Max_MP),
@@ -108,7 +191,15 @@ CREATE TABLE StatModifier(
     FOREIGN KEY (Attack_modifier) REFERENCES Status(Attack),
     FOREIGN KEY (Accuracy_modifier) REFERENCES Status(Accuracy),
     FOREIGN KEY (Defense_modifier) REFERENCES Status(Defense),
-    FOREIGN KEY (Evasion_modifier) REFERENCES Status(Evasion)
+    FOREIGN KEY (Evasion_modifier) REFERENCES Status(Evasion) */
+);
+
+CREATE TABLE StatusStatModifier (
+    Status_ID INTEGER,
+    StatModValue INTEGER,
+    FOREIGN KEY (Status_ID) REFERENCES Status(Status_ID),
+    FOREIGN KEY (StatModValue) REFERENCES StatModifier(StatModValue),
+    PRIMARY KEY (Status_ID, StatModValue)
 );
 
 create table Entity(
@@ -118,6 +209,22 @@ Afflicted_Status_effects TEXT,
 Equipment TEXT,
 Status_ID INTEGER,
 FOREIGN KEY (Status_ID) REFERENCES Status(Status_ID)
+);
+
+create table Team(
+TeamID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+Name text,
+Entity_ID INTEGER,
+foreign key (Entity_ID) REFERENCES Entity(Entity_ID)
+);
+
+CREATE TABLE BattleMaster (
+    Battle_ID INTEGER PRIMARY KEY NOT NULL,
+    Name TEXT,
+    Special_Event TEXT,
+    TeamID INTEGER NOT NULL,
+    Afflicted_Status_effects TEXT,
+    FOREIGN KEY (TeamID) REFERENCES Team(TeamID)
 );
 
 --FAKE CODE VERSION BELOW
