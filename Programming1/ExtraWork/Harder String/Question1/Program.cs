c9bf842fe16421e199f5ddb99d0a245afb1c9cc0
OLDILJ <oldilj1@student.op.ac.nz>
2024-04-11 11:33:43 +1200

    Finished Harder String

---
+++ Programming1/ExtraWork/Harder String/Question1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+Step 1: Get string from user
+Step 2: Split String. 
+Step 3: Compare Split.length to find the longest and smallest word.
+*/
+Console.WriteLine("PLEASE ENTER A SENTENCE");
+string sentence = Console.ReadLine();
+string[] splitsentence = sentence.Split(' ');
+Array.Sort(splitsentence, (x, y) => x.Length.CompareTo(y.Length));
+/*
+foreach (string test in splitsentence)
+{
+    Console.WriteLine(test);
+}
+*/
+var first = splitsentence.First();
+var last = splitsentence.Last();
+Console.WriteLine($"The Longest word in the sentence is {last}");
+Console.WriteLine($"The Shortest word in the sentence is {first}");
---
+++ Programming1/ExtraWork/Harder String/Question3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string sentence = Console.ReadLine();
+char[] chararray = sentence.ToCharArray();
+char[] reversed = chararray.Reverse().ToArray();
+string rsentence = new string(reversed);
+
+if (rsentence == sentence)
+{
+    Console.WriteLine("PALINDROME DETECTED :)");
+    Console.WriteLine($"{rsentence} is a palindrome of {sentence}");
+}
+else
+{
+    Console.WriteLine("No PALINDROME Found :(");
+}
