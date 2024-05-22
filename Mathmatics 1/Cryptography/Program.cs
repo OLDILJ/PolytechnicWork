0cb83f570562db40becb88fdc975c9827c305e53
Logan Olding <OLDILJ1@student.op.ac.nz>
2024-05-22 12:03:09 +1200

    Implemented Weird Inverse Method

--- Mathmatics 1/Cryptography/Program.cs
+++ Mathmatics 1/Cryptography/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 int AsciCode = 0;
 int AsciConvertedUp;
 int AsciConvertedDown;
@@ -9,6 +8,15 @@ string UserChoice;
 string UserString;
 int FirstInt;
 int SecondInt;
+int ThirdInt;
+int FourthInt;
+double FirstDouble;
+double SecondDouble;
+double ThirdDouble;
+double FourthDouble;
+int FirstInverse = 0;
+int DummyNumber1;
+int DummyNumber2;
 /*
 for (int i = 0; i < alpha.Length; i++)
 {
@@ -33,6 +41,9 @@ Console.WriteLine("Now Select What Cipher You Want To Apply");
 Console.WriteLine("1. Caeser (+3)");
 Console.WriteLine("2. Undo Caeser (-3)");
 Console.WriteLine("3. Affine (aP + b)");
+Console.WriteLine("4. Undo Affine (aP + b)");
+Console.WriteLine("5. Hill (C1 = (aP1 + bP2)mod26) (C2 = (cP1 + dP2)mod26)");
+Console.WriteLine("6. Undo Hill (C1 = (aP1 + bP2)mod26) (C2 = (cP1 + dP2)mod26)");
 UserChoice = Console.ReadLine();
 switch (UserChoice)
 {
@@ -78,6 +89,58 @@ switch (UserChoice)
         }
         break;
     case "4":
+    Console.WriteLine("Please Give The First Int");
+    FirstDouble = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Please Give The Second Int");
+    SecondDouble = Convert.ToDouble(Console.ReadLine());
+
+    for (int o = 0; o < 26; o++)
+    {
+        if (((FirstDouble*o) % 26) == 1)
+        {
+            Console.WriteLine($"InverseFound{o}");
+            FirstInverse = o;
+            break;
+        }
+    //Console.Write($"Pass{o} ");
+    }
+    for (int i = 0; i < UserChar.Length; i++)
+    {
+        AsciCode = (int)UserChar[i];
+        Console.Write($" (Original AsciValue {AsciCode}) ");
+        Console.Write($" (Double {FirstDouble})");
+        AsciConverted = Convert.ToInt32(((FirstInverse)*((AsciCode - 65) - SecondDouble))%26);
+        if (AsciConverted < 0)
+        {
+            AsciConverted = -AsciConverted;
+        }
+        Console.Write($" (Decoded {AsciConverted}) ");
+        AsciConverted = (AsciConverted + 65);
+        Console.Write($" (End AsciValue {AsciConverted}) ");
+        char AsciChar = (char) (AsciConverted);
+        Console.WriteLine(AsciChar);
+        
+    }
+        break;
+    case "5":
+        Console.WriteLine("Please Give The First Int");
+        FirstInt = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please Give The Second Int");
+        SecondInt = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < UserChar.Length; i++)
+        {
+            AsciCode = (int)UserChar[i];
+            AsciConverted = (((FirstInt * (AsciCode - 65)) + SecondInt) % 26);
+            char AsciChar = (char) (AsciConverted + 65);
+            Console.Write(AsciChar);
+            //Console.Write(AsciConverted);
+        }
+        break;
+    case "6":
+    //if det outside 25 mod 26
+    //
+    //
+    //    
         Console.WriteLine("Please Give The First Int");
         FirstInt = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Please Give The Second Int");
---
+++ Mathmatics 1/testenvironment/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int AsciCode = 0;
+int AsciConvertedUp;
+int AsciConvertedDown;
+int AsciConverted;
+char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+string UserChoice;
+string UserString;
+int FirstInt;
+int SecondInt;
+int ThirdInt;
+int FourthInt;
+double FirstDouble;
+double SecondDouble;
+double ThirdDouble;
+double FourthDouble;
+UserString = Console.ReadLine();
+UserString = UserString.ToUpper();
+char[] UserChar = UserString.ToCharArray();
+int FirstInverse = 0;
+int DummyNumber1;
+int DummyNumber2;
+/*for (int i = 0; i < UserChar.Length; i++)
+        {
+            AsciCode = (int)UserChar[i];
+            AsciConverted = (AsciCode);
+            char AsciChar = (char) AsciConverted;
+            //Console.Write(AsciChar);
+            Console.Write(AsciConverted);
+        }
+*/
+Console.WriteLine("Please Give The First Int");
+FirstDouble = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please Give The Second Int");
+SecondDouble = Convert.ToDouble(Console.ReadLine());
+
+for (int o = 0; o < 26; o++)
+{
+    if (((FirstDouble*o) % 26) == 1)
+    {
+        Console.WriteLine($"InverseFound{o}");
+        FirstInverse = o;
+        break;
+    }
+    Console.Write($"Pass{o} ");
+}
+for (int i = 0; i < UserChar.Length; i++)
+{
+    AsciCode = (int)UserChar[i];
+    Console.Write($" (Original AsciValue {AsciCode}) ");
+    Console.WriteLine($" (Double {FirstDouble})");
+    AsciConverted = Convert.ToInt32(((FirstInverse)*((AsciCode - 65) - SecondDouble))%26);
+    Console.Write($" (Decoded {AsciConverted}) ");
+    AsciConverted = (AsciConverted + 65);
+    Console.Write($" (End AsciValue {AsciConverted}) ");
+    char AsciChar = (char) (AsciConverted);
+    //Console.Write(AsciChar);
+    
+}
