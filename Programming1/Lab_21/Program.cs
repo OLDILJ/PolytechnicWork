13fb1fdee1ecd451e34fa5fdfa2a408fb1d40770
OLDILJ <OLDILJ1@op.ac.nz>
2024-05-30 11:50:46 +1200

    added Reading/Writing To File in Lab_22 and Assignment

--- Programming1/Lab_21/Program.cs
+++ Programming1/Lab_21/Program.cs
@@ -11,9 +11,28 @@
         static readonly string textFile = @"names.txt";
         static void Main(string[] args)
         {
+            //Console.WriteLine("ClosingFile");
+
+            FamFeudPlayers[] Players = new FamFeudPlayers[27];
             //vector<string> FamFeudPlayers;
+            int PlayerCharactersMade = 0;
             int StartedGame = 0;
             string PlayerChoice;
+            StreamReader si = new StreamReader("interest.txt");
+            for (int i = 0;i <= 26; i++)
+            {
+                //Players[i].FName = sr.ReadLine();
+                //Players[i].LName = sr.ReadLine();
+                //Players[i].Address = "Dunedin";
+                Players[i].Interest = si.ReadLine();
+                //Console.WriteLine(Players[i].FName);
+                //Console.WriteLine(Players[i].LName);
+                //Console.WriteLine(Players[i].Address);
+                //Console.WriteLine(Players[i].team);
+                //i++;
+            }
+            si.Close();
+
             Console.WriteLine("Welcome To Family Feud");
             Console.WriteLine("Before We Begin Please Type {Y} To Take Part In This Game");
             PlayerChoice = Console.ReadLine();
@@ -21,15 +40,18 @@
             {
                 case "y" or "Y":
                     Console.WriteLine("WELCOME");
-                    Console.WriteLine("Do You Want To Manually Define Your Players?");
+                    Console.WriteLine("Type {Y} To Manually Define Your Players");
                     PlayerChoice = Console.ReadLine();
                     switch(PlayerChoice)
                     {
                         case "y" or "Y":
+                        PlayerCharactersMade = 2;
                         PlayerDefining();
+                        StartGame();
                         break;
                         default:
                         TextReader();
+                        StartGame();
                         break;
 
 
@@ -45,22 +67,59 @@
 
             void PlayerDefining()
             {
-                FamFeudPlayers[] Players = new FamFeudPlayers[5];
-    
-                for (int i = 0; i < Players.Length; i++)
-                {
-                    Console.WriteLine("MaorPeople");
-                }
+                //step 1 create a characters details
+                //step 2 increment the made characters by 2 (to cover first and last name)
+                //step 3 ask if they want to make another character
+                Console.WriteLine($"Please enter Contestant #{PlayerCharactersMade-1}'s FIRST Name");
+                Players[PlayerCharactersMade-1].FName = Console.ReadLine();
+                Console.WriteLine($"Please enter Contestant #{PlayerCharactersMade-1}'s LAST Name");
+                Players[PlayerCharactersMade-1].LName = Console.ReadLine();
+                Console.WriteLine($"Please enter Contestants #{PlayerCharactersMade-1}'s INTEREST");
+                Players[PlayerCharactersMade-1].Interest = Console.ReadLine();
+
+
 
             }
             void TextReader()
             {
-                string[] lines = File.ReadAllLines(textFile);
-
-                foreach (string line in lines)
+            StreamReader sr = new StreamReader("names.txt");
+            for (int i = (PlayerCharactersMade);i <= 26; i++)
+            {
+                Players[i].FName = sr.ReadLine();
+                Players[i].LName = sr.ReadLine();
+                //Players[i].Address = "Dunedin";
+                //Players[i].team = rand.Next(1,5);
+                Console.WriteLine(Players[i].FName);
+                Console.WriteLine(Players[i].LName);
+                //Console.WriteLine(Players[i].Address);
+                //Console.WriteLine(Players[i].team);
+                //i++;
+            }
+            sr.Close();
+            Console.WriteLine("ClosingFile");
+            }
+            void CustomDisplay()
+            {
+                Console.WriteLine("TEST");
+                
+                for (int i = 0;i <= 26; i++)
                 {
-                    Console.WriteLine(line);
+                    Console.Write(Players[i].FName.PadRight(10));
+                    Console.Write(Players[i].LName.PadLeft(20));
+                    Console.WriteLine(Players[i].Interest.PadLeft(30));
+                    //Console.WriteLine(Convert.ToString(students[i].team).PadLeft(20));
+                    //Console.WriteLine(i);
                 }
+            }
+            //sr.Close();
+            void StartGame()
+            {
+                Console.WriteLine("WELCOME TO FAMILY FEUD");
+                Console.WriteLine("IM YOUR HOST FEUD GUY");
+                Console.WriteLine("HERE ARE OUR CONTESTANTS FOR THE DAY");
+                CustomDisplay();
+
+
             }
 
 
@@ -92,3 +151,57 @@
     }
 
 }
+/*
+            Students[] students = new Students[27];
+            Random rand = new Random();
+            StreamReader sr = new StreamReader("names.txt");
+
+
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                students[i].FName = sr.ReadLine();
+                students[i].LName = sr.ReadLine();
+                students[i].Address = "Dunedin";
+                students[i].team = rand.Next(1,5);
+                //Console.WriteLine(students[i].FName);
+                //Console.WriteLine(students[i].LName);
+                //Console.WriteLine(students[i].Address);
+                //Console.WriteLine(students[i].team);
+                i++;
+            }
+            
+
+
+
+
+
+            //sr.Close();
+            CustomDisplay();
+
+            void CustomDisplay()
+            {
+                Console.WriteLine("TEST");
+                
+                for (int i = 0;i <= 26; i++)
+                {
+                    Console.Write(students[i].FName.PadRight(10));
+                    Console.Write(students[i].LName.PadLeft(20));
+                    Console.Write(students[i].Address.PadLeft(20));
+                    Console.WriteLine(Convert.ToString(students[i].team).PadLeft(20));
+                    //Console.WriteLine(i);
+                }
+            }
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("ourClass.txt");
+            for (int o = 0;o <= 26; o++)
+                {
+                    sw.Write(students[o].FName.PadRight(10));
+                    sw.Write(students[o].LName.PadLeft(20));
+                    sw.Write(students[o].Address.PadLeft(20));
+                    sw.WriteLine(Convert.ToString(students[o].team).PadLeft(20));
+                    //Console.WriteLine(i);
+                }
+            sw.Close();
+*/
---
+++ Programming1/Lab_22/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//StreamWriter sw = new StreamWriter(@"../../../test.txt");
+//StreamWriter sw = new StreamWriter(@"test.txt");
+//StreamReader sr = new StreamReader("names.txt");
+//sw.Close();
+
+
+namespace testProject
+{
+    public struct Students
+    {
+        public string FName;
+        public string LName;
+        public string Address;
+        public int team;
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Students[] students = new Students[27];
+            Random rand = new Random();
+            StreamReader sr = new StreamReader("names.txt");
+
+
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                students[i].FName = sr.ReadLine();
+                students[i].LName = sr.ReadLine();
+                students[i].Address = "Dunedin";
+                students[i].team = rand.Next(1,5);
+                //Console.WriteLine(students[i].FName);
+                //Console.WriteLine(students[i].LName);
+                //Console.WriteLine(students[i].Address);
+                //Console.WriteLine(students[i].team);
+                i++;
+            }
+            
+
+
+
+
+
+            //sr.Close();
+            CustomDisplay();
+
+            void CustomDisplay()
+            {
+                Console.WriteLine("TEST");
+                
+                for (int i = 0;i <= 26; i++)
+                {
+                    Console.Write(students[i].FName.PadRight(10));
+                    Console.Write(students[i].LName.PadLeft(20));
+                    Console.Write(students[i].Address.PadLeft(20));
+                    Console.WriteLine(Convert.ToString(students[i].team).PadLeft(20));
+                    //Console.WriteLine(i);
+                }
+            }
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("ourClass.txt");
+            for (int o = 0;o <= 26; o++)
+                {
+                    sw.Write(students[o].FName.PadRight(10));
+                    sw.Write(students[o].LName.PadLeft(20));
+                    sw.Write(students[o].Address.PadLeft(20));
+                    sw.WriteLine(Convert.ToString(students[o].team).PadLeft(20));
+                    //Console.WriteLine(i);
+                }
+            sw.Close();
+
+
+
+        }
+    static void FIN()
+    {
+        Console.ReadLine();
+    }
+    }
+
+}
