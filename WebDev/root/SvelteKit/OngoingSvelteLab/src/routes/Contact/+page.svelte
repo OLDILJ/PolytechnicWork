7838e99518518c86ee8641a318ec4e90c8ef2494
OLDILJ <OLDILJ1@op.ac.nz>
2024-10-15 11:24:09 +1300

    Set up Contact Page Layout

---
+++ WebDev/root/SvelteKit/OngoingSvelteLab/src/routes/Contact/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import Head from "$lib/Header.svelte";
+    export let data: PageData;
+</script>
+
+
+<Head headerTitle = "Contact"/>
+
+<style></style>
