42bf79c7487998014d8bb0291a91369b608514a9
OLDILJ <OLDILJ1@op.ac.nz>
2024-10-15 11:20:17 +1300

    Set up About + setup Header

---
+++ WebDev/root/SvelteKit/OngoingSvelteLab/src/routes/About/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import Head from "$lib/Header.svelte";
+    
+    export let data: PageData;
+</script>
+
+<Head headerTitle = "About"/>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore nulla ab voluptatem iste tenetur voluptates debitis hic iusto. Sequi ducimus fugiat delectus reiciendis alias. Provident quisquam nemo voluptate veniam ad?
+Atque fugiat delectus veniam optio assumenda obcaecati. Aliquam, maxime cupiditate! Quas molestiae aut fugiat, velit fugit fuga cupiditate dolor adipisci, minima placeat illum tempore asperiores commodi praesentium necessitatibus vero impedit!</p>
+
+<style>
+
+</style>
