d7ab61369e97aca771b3a49dae1292443dce4f5f
OLDILJ <OLDILJ1@op.ac.nz>
2025-10-10 14:13:34 +1300

    Created Basic Left Right Movement

--- IntermediateAppDev/2D Platformer/new-game-project/PlayerController.gd
+++ IntermediateAppDev/2D Platformer/new-game-project/PlayerController.gd
@@ -4,18 +4,24 @@ class_name PlayerController
 
 @export var Player: CharacterBody2D
 
-@export var moveSpeed := 10
+@export var moveSpeed := 100
 
 var moveDirection : Vector2
+var friction = 0.4
+
 
 func PhysicsUpdate(delta: float):
-	var velocity = Vector2.ZERO
+	
 	if Input.is_action_pressed("Right"):
 		print("Sending Right Signal")
-		Transition.emit(self, "right")
+		moveDirection = Vector2(1,0)
 	if Input.is_action_pressed("Left"):
 		print("Sending Left Signal")
-		Transition.emit(self, "left")
+		moveDirection = Vector2(-1,0)
 	if Input.is_action_pressed("Jump"):
 		print("Sending Jump Signal")
 		Transition.emit(self, "jump")
+	
+	
+	if Player:
+		Player.velocity = moveDirection * moveSpeed
--- IntermediateAppDev/2D Platformer/new-game-project/State/Player.gd
+++ IntermediateAppDev/2D Platformer/new-game-project/State/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # reminder that all movement stuff is done in States Check STATEMACHINE.gd and State.gd for more info
 func _physics_process(delta):
+
 	move_and_slide()
 	
 	if velocity.length() > 0 && velocity.y == 0:
