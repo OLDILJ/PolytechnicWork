b60b82a713074ca8eeb9666bd0bcb2a5973d4d90
OLDILJ <OLDILJ1@op.ac.nz>
2025-10-16 09:56:52 +1300

    Finishing Touches

--- IntermediateAppDev/2D Platformer/new-game-project/State/PlayerJump.gd
+++ IntermediateAppDev/2D Platformer/new-game-project/State/PlayerJump.gd
@@ -4,7 +4,6 @@ class_name PlayerJump
 
 @export var Player: CharacterBody2D
 @export var AnimationSprite: AnimatedSprite2D
-@export var LandingLagTimer: Timer
 @export var moveSpeed := 100
 @export var JumpForce := 400
 @export var AirStrafeSpeed := 5
@@ -18,9 +17,9 @@ func PhysicsUpdate(delta: float):
 	if AnimationSprite.frame == 4:
 		AnimationSprite.play("Fall")
 	if Input.is_action_pressed("Left"):
-		Player.velocity.x = Player.velocity.x - 5
+		Player.velocity.x = Player.velocity.x - AirStrafeSpeed
 	if Input.is_action_pressed("Right"):
-		Player.velocity.x = Player.velocity.x + 5
+		Player.velocity.x = Player.velocity.x + AirStrafeSpeed
 	if Player.is_on_floor():
 		Transition.emit(self, "Default")
 		
