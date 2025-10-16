8056a07c0f4b8f652e5c0b2b5f5f3b7bfa455f1f
OLDILJ <OLDILJ1@op.ac.nz>
2025-10-16 13:37:04 +1300

    Added Hitboxes and Stomp Attack

--- IntermediateAppDev/2D Platformer/new-game-project/character_body_2d.gd
+++ IntermediateAppDev/2D Platformer/new-game-project/character_body_2d.gd
@@ -5,6 +5,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		#print("Applying Gravity")
 		velocity.y += delta * Gravity
-
-
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("collided")
+	if area.is_in_group("Stomp"):
+		queue_free()
