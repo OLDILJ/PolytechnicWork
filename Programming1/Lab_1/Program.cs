75e3936e778fdaa2ce19580dc4de12122831ae5c
OLDILJ <OLDILJ1@op.ac.nz>
2026-03-17 09:08:08 +1300

    Cleaned up Repo and added OE2

--- Programming1/Lab_1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.BackgroundColor = ConsoleColor.DarkGreen;
-Console.ForegroundColor = ConsoleColor.Gray;
-Console.WriteLine("* * * * * * * * * * * *");
-Console.WriteLine("* Logan J. Olding     *");
-Console.WriteLine("* 40 Beaconsfield RD  *");
-Console.WriteLine("* My favorite BIT     *");
-Console.WriteLine("* Subject is :idk yet *");
-Console.WriteLine("* * * * * * * * * * * *");
-Console.ReadLine();
